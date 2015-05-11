711505e06a95ee5c0a548c163cb724546ff9ac3c
vorobjevvalera <vorobjevvalera@gmail.com>
2015-05-11 15:18:49 +0300

    Add class TradeEndpoints

---
+++ Rabun.Oanda.Rest/Endpoints/TradeEndpoints.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rabun.Oanda.Rest.Base;
+
+namespace Rabun.Oanda.Rest.Endpoints
+{
+    public class TradeEndpoints: Endpoint
+    {
+        public TradeEndpoints(string key, AccountType accountType) : base(key, accountType)
+        {
+        }
+    }
+}
