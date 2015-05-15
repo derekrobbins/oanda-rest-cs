3e714601fb72e155379e5f14261deb265773d544
vorobjevvalera <vorobjevvalera@gmail.com>
2015-05-15 14:06:18 +0300

    Added DefaultFactory and test for one

---
+++ Rabun.Oanda.Rest.Test/FactoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rabun.Oanda.Rest.Base;
+using Rabun.Oanda.Rest.Endpoints;
+using Rabun.Oanda.Rest.Factories;
+using Rabun.Oanda.Rest.Models;
+
+namespace Rabun.Oanda.Rest.Test
+{
+    [TestClass]
+    public class FactoryTest
+    {
+        [TestMethod]
+        public async Task DefaultFactoryTest()
+        {
+            DefaultFactory factory = new DefaultFactory("ec89b162d4a9922c8fa40769c2453d8b-cc1fb522857d46a08a90ef09730343a6", AccountType.practice, 4905675);
+            RateEndpoints rateEndpoints = factory.GetEndpoint<RateEndpoints>();
+
+            Assert.IsNotNull(rateEndpoints);
+
+            List<Price> prices = await rateEndpoints.GetPrices("EUR_USD");
+            Assert.IsNotNull(prices);
+            Assert.IsTrue(prices.Count > 0);
+        }
+    }
+}
---
+++ Rabun.Oanda.Rest/Factories/DefaultFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rabun.Oanda.Rest.Base;
+
+namespace Rabun.Oanda.Rest.Factories
+{
+    /// <summary>
+    /// Default factory for all endpoints
+    /// </summary>
+    public class DefaultFactory
+    {
+        private readonly string _key;
+        private readonly AccountType _accountType;
+        private readonly int _accountId;
+
+        /// <summary>
+        /// Public constructor
+        /// </summary>
+        /// <param name="key">Oanda account private key</param>
+        /// <param name="accountType">Account type (practice or real)</param>
+        /// <param name="accountId">Account Id</param>
+        public DefaultFactory(string key, AccountType accountType, int accountId)
+        {
+            if (string.IsNullOrWhiteSpace(key)) throw new NullReferenceException("Key don't be null or empry");
+
+            _key = key;
+            _accountType = accountType;
+            _accountId = accountId;
+        }
+
+        /// <summary>
+        /// Make endpoints instance. Use generic method.
+        /// </summary>
+        /// <typeparam name="T">Endpoint type</typeparam>
+        /// <returns></returns>
+        public T GetEndpoint<T>() where T : Endpoint
+        {
+            Type t = typeof (T);
+
+            return (T) Activator.CreateInstance(t, _key, _accountType, _accountId);
+        }
+    }
+}
